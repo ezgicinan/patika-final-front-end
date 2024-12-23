ef587df5aa36f7ec7503d8dadac958f93dd50c02
Ezgi Cinan <ezgi.cinan@marun.edu.tr>
2024-12-24 02:22:44 +0300

    front implementation

--- simple-e-commerce/src/services/productService.jsx
+++ simple-e-commerce/src/services/productService.jsx
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 export const fetchProducts = () => {
     try{
-        return axios.get('https://fakestoreapi.com/products');
+        //return axios.get('https://fakestoreapi.com/products');
+        console.log('fetching products');
+        return axios.get('http://localhost:8000/api/product'); 
     }catch (error) {
         console.error('Error fetching products:', error);
         throw error;
